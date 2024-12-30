4d77f943657fff2590adadd7292049044e04f3f0
mohora11 <mohora@naver.com>
2024-12-30 19:38:23 +0900

    단일 symbol에서 멀티 symbol Kline 회신

--- api/test_api.go
+++ api/test_api.go
@@ -4,186 +4,340 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
 
 	bsml "krononlabs/busimodel"
 	utility "krononlabs/utility"
+
+	"github.com/gorilla/websocket"
 )
 
 const (
 	binanceKline = "https://api.binance.com/api/v3/klines"
 	bitgetKline  = "https://api.bitget.com/api/v2/spot/market/history-candles"
+
+	binanWSKline = ""
+	bitgtWSKline = ""
 )
 
 // Kline 통합 조회
-func KlineInq(ReqInfo *bsml.KlineRequest) (bool, string, []bsml.KlineResp) {
+func KlineInq(ReqInfo *bsml.KlineRequest) (bool, string, map[string][]bsml.KlineResp) {
 
 	var (
-		resp []bsml.KlineResp
-
-		url string
+		respMap = make(map[string][]bsml.KlineResp) // 심볼별로 응답을 저장하는 맵
+		wg      sync.WaitGroup                      // 동시 요청을 처리하기 위한 WaitGroup
+		mu      sync.Mutex                          // 여러 고루틴에서 공유하는 resp를 안전하게 수정하기 위한 Mutex
 	)
 
 	// 거래소별 interval 값 변환
 	interval := utility.GetExchangeInterval(ReqInfo.Interval, ReqInfo.ExchgNm)
 
-	if ReqInfo.ExchgNm == "binance" {
-		// 바이낸스 Kline API 요청 URL 생성
-		url = fmt.Sprintf("%s?symbol=%s&interval=%s", binanceKline, ReqInfo.Symbol, interval)
-	} else if ReqInfo.ExchgNm == "bitget" {
-		// 비트겟 Kline API 요청 URL 생성
-		url = fmt.Sprintf("%s?symbol=%s&granularity=%s", bitgetKline, ReqInfo.Symbol, interval)
-	}
+	// Symbols 배열에 대해 각각의 요청을 병렬 처리
+	for _, symbol := range ReqInfo.Symbols { // 이제 Symbols는 배열로 가정
+		wg.Add(1)
+		go func(symbolStr string) {
+			defer wg.Done()
 
-	// 선택적인 값이 존재하면 URL에 추가
-	if ReqInfo.StartTime != "" {
+			var url string
 
-		// 날짜 문자열을 time.Time 객체로 변환
-		start, err := time.Parse(time.RFC3339, ReqInfo.StartTime)
-		if err != nil {
-			fmt.Println("Error parsing start time:", err)
-
-			return false, "time parse error", resp
-		}
-
-		startUnix := start.Unix() * 1000
-
-		url = fmt.Sprintf("%s&startTime=%d", url, startUnix)
-	}
-
-	if ReqInfo.EndTime != "" {
-
-		end, err := time.Parse(time.RFC3339, ReqInfo.EndTime)
-		if err != nil {
-			fmt.Println("Error parsing end time:", err)
-
-			return false, "time parse error", resp
-		}
-
-		endUnix := end.Unix() * 1000
-
-		url = fmt.Sprintf("%s&endTime=%d", url, endUnix)
-	}
-
-	if ReqInfo.Limit > 0 {
-		url = fmt.Sprintf("%s&limit=%d", url, ReqInfo.Limit)
-	}
-
-	if ReqInfo.TimeZone != "" {
-		url = fmt.Sprintf("%s&timeZone=%s", url, ReqInfo.TimeZone)
-	}
-
-	// HTTP GET 요청 보내기
-	httpResp, err := http.Get(url)
-	if err != nil {
-		return false, "Request failed", resp
-	}
-	defer httpResp.Body.Close()
-
-	body, err := io.ReadAll(httpResp.Body)
-	if err != nil {
-		return false, "", nil
-	}
-
-	// 응답 JSON 출력
-	fmt.Println("Response body:", string(body))
-
-	// 거래소에 따라 응답을 다르게 처리
-	if ReqInfo.ExchgNm == "binance" {
-		// 바이낸스 응답 처리
-		var rawData [][]interface{}
-		if err := json.Unmarshal(body, &rawData); err != nil {
-			return false, fmt.Sprintf("Error unmarshaling Binance response: %v", err), nil
-		}
-
-		// rawData를 KlineResp 구조체로 변환
-		for _, kline := range rawData {
-			if len(kline) < 6 {
-				// 최소 6개의 필드가 있어야 함
-				continue
+			// 거래소별 Kline API 요청 URL 생성
+			if ReqInfo.ExchgNm == "binance" {
+				// 바이낸스 Kline API 요청 URL 생성
+				url = fmt.Sprintf("%s?symbol=%s&interval=%s", binanceKline, symbolStr, interval)
+			} else if ReqInfo.ExchgNm == "bitget" {
+				// 비트겟 Kline API 요청 URL 생성
+				url = fmt.Sprintf("%s?symbol=%s&granularity=%s", bitgetKline, symbolStr, interval)
 			}
 
-			// 공통 필드들
-			timestamp := int64(kline[0].(float64))
-			open := kline[1].(string)
-			high := kline[2].(string)
-			low := kline[3].(string)
-			close := kline[4].(string)
-			volume := kline[5].(string)
-			closeTime := int64(kline[6].(float64))    // closeTime
-			quoteVolume := kline[7].(string)          // quoteVolume 사용
-			numberOfTrades := int(kline[8].(float64)) // numberOfTrades
-			takerBuyBaseVolume := kline[9].(string)   // takerBuyBaseVolume
-			takerBuyQuoteVolume := kline[10].(string) // takerBuyQuoteVolume
+			// 선택적인 값이 존재하면 URL에 추가
+			if ReqInfo.StartTime != "" {
+				// 날짜 문자열을 time.Time 객체로 변환
+				start, err := time.Parse(time.RFC3339, ReqInfo.StartTime)
+				if err != nil {
+					fmt.Println("Error parsing start time:", err)
+					return
+				}
+				startUnix := start.Unix() * 1000
 
-			// KlineResp 구조체에 데이터를 할당
-			resp = append(resp, bsml.KlineResp{
-				Timestamp:           timestamp,
-				Open:                utility.ParseToFloat64(open),
-				High:                utility.ParseToFloat64(high),
-				Low:                 utility.ParseToFloat64(low),
-				Close:               utility.ParseToFloat64(close),
-				Volume:              utility.ParseToFloat64(volume),
-				CloseTime:           closeTime,                                   // 바이낸스에는 CloseTime이 없으므로 0
-				QuoteVolume:         utility.ParseToFloat64(quoteVolume),         // 바이낸스에서 quoteVolume은 사용하지 않으므로 0으로 처리
-				NumberOfTrades:      numberOfTrades,                              // 바이낸스에는 NumberOfTrades가 없으므로 0
-				TakerBuyBaseVolume:  utility.ParseToFloat64(takerBuyBaseVolume),  // 바이낸스에는 TakerBuyBaseVolume이 없음
-				TakerBuyQuoteVolume: utility.ParseToFloat64(takerBuyQuoteVolume), // 바이낸스에는 TakerBuyQuoteVolume이 없음
-			})
-		}
-	} else if ReqInfo.ExchgNm == "bitget" {
-		// 비트겟 응답 처리
-		var rawData struct {
-			Code        string          `json:"code"`
-			Msg         string          `json:"msg"`
-			RequestTime int64           `json:"requestTime"`
-			Data        [][]interface{} `json:"data"`
-		}
-
-		if err := json.Unmarshal(body, &rawData); err != nil {
-			return false, fmt.Sprintf("Error unmarshaling Bitget response: %v", err), nil
-		}
-
-		// rawData를 KlineResp 구조체로 변환
-		for _, kline := range rawData.Data {
-			if len(kline) < 6 {
-				// 최소 6개의 필드가 있어야 함
-				continue
+				url = fmt.Sprintf("%s&startTime=%d", url, startUnix)
 			}
 
-			// 공통 필드들 (비트겟은 timestamp가 string으로 반환됨)
-			timestampStr := kline[0].(string)
-			timestamp, err := strconv.ParseInt(timestampStr, 10, 64) // string을 int64로 변환
+			if ReqInfo.EndTime != "" {
+				end, err := time.Parse(time.RFC3339, ReqInfo.EndTime)
+				if err != nil {
+					fmt.Println("Error parsing end time:", err)
+					return
+				}
+				endUnix := end.Unix() * 1000
+				url = fmt.Sprintf("%s&endTime=%d", url, endUnix)
+			}
+
+			if ReqInfo.Limit > 0 {
+				url = fmt.Sprintf("%s&limit=%d", url, ReqInfo.Limit)
+			}
+
+			if ReqInfo.TimeZone != "" {
+				url = fmt.Sprintf("%s&timeZone=%s", url, ReqInfo.TimeZone)
+			}
+
+			fmt.Println(url)
+
+			// HTTP GET 요청 보내기
+			httpResp, err := http.Get(url)
 			if err != nil {
-				fmt.Println("Error parsing timestamp:", err)
-				continue
+				fmt.Println("Request failed for symbol:", symbol)
+				return
 			}
-			open := kline[1].(string)
-			high := kline[2].(string)
-			low := kline[3].(string)
-			close := kline[4].(string)
-			volume := kline[5].(string)
+			fmt.Println(httpResp)
+			defer httpResp.Body.Close()
 
-			// KlineResp 구조체에 데이터를 할당
-			resp = append(resp, bsml.KlineResp{
-				Timestamp:           timestamp,
-				Open:                utility.ParseToFloat64(open),
-				High:                utility.ParseToFloat64(high),
-				Low:                 utility.ParseToFloat64(low),
-				Close:               utility.ParseToFloat64(close),
-				Volume:              utility.ParseToFloat64(volume),
-				CloseTime:           0,   // 비트겟에는 CloseTime이 없음
-				QuoteVolume:         0.0, // 비트겟에서는 quoteVolume을 사용할 수 없으므로 0으로 처리
-				NumberOfTrades:      0,   // 비트겟에는 NumberOfTrades가 없음
-				TakerBuyBaseVolume:  0.0, // 비트겟에는 TakerBuyBaseVolume이 없음
-				TakerBuyQuoteVolume: 0.0, // 비트겟에는 TakerBuyQuoteVolume이 없음
-			})
-		}
+			body, err := io.ReadAll(httpResp.Body)
+			fmt.Println(body)
+			if err != nil {
+				fmt.Println("Error reading response body for symbol:", symbol)
+				return
+			}
+
+			// 거래소에 따라 응답을 다르게 처리
+			if ReqInfo.ExchgNm == "binance" {
+				// 바이낸스 응답 처리
+				var rawData [][]interface{}
+				if err := json.Unmarshal(body, &rawData); err != nil {
+					fmt.Println("Error unmarshaling Binance response:", err)
+					return
+				}
+
+				// rawData를 KlineResp 구조체로 변환
+				var respData []bsml.KlineResp
+				for _, kline := range rawData {
+					if len(kline) < 11 {
+						// 최소 11개의 필드가 있어야 함
+						continue
+					}
+
+					// 공통 필드들
+					timestamp := int64(kline[0].(float64))
+					open := kline[1].(string)
+					high := kline[2].(string)
+					low := kline[3].(string)
+					close := kline[4].(string)
+					volume := kline[5].(string)
+					closeTime := int64(kline[6].(float64))    // closeTime
+					quoteVolume := kline[7].(string)          // quoteVolume 사용
+					numberOfTrades := int(kline[8].(float64)) // numberOfTrades
+					takerBuyBaseVolume := kline[9].(string)   // takerBuyBaseVolume
+					takerBuyQuoteVolume := kline[10].(string) // takerBuyQuoteVolume
+
+					// KlineResp 구조체에 데이터를 할당
+					respData = append(respData, bsml.KlineResp{
+						Timestamp:           timestamp,
+						Open:                utility.ParseToFloat64(open),
+						High:                utility.ParseToFloat64(high),
+						Low:                 utility.ParseToFloat64(low),
+						Close:               utility.ParseToFloat64(close),
+						Volume:              utility.ParseToFloat64(volume),
+						CloseTime:           closeTime,                                   // 바이낸스에는 CloseTime이 없으므로 0
+						QuoteVolume:         utility.ParseToFloat64(quoteVolume),         // 바이낸스에서 quoteVolume은 사용하지 않으므로 0으로 처리
+						NumberOfTrades:      numberOfTrades,                              // 바이낸스에는 NumberOfTrades가 없으므로 0
+						TakerBuyBaseVolume:  utility.ParseToFloat64(takerBuyBaseVolume),  // 바이낸스에는 TakerBuyBaseVolume이 없음
+						TakerBuyQuoteVolume: utility.ParseToFloat64(takerBuyQuoteVolume), // 바이낸스에는 TakerBuyQuoteVolume이 없음
+					})
+				}
+				// 심볼별로 응답을 맵에 추가
+				mu.Lock()
+				respMap[symbolStr] = respData
+				mu.Unlock()
+			} else if ReqInfo.ExchgNm == "bitget" {
+				// 비트겟 응답 처리
+				var rawData struct {
+					Code        string          `json:"code"`
+					Msg         string          `json:"msg"`
+					RequestTime int64           `json:"requestTime"`
+					Data        [][]interface{} `json:"data"`
+				}
+
+				if err := json.Unmarshal(body, &rawData); err != nil {
+					fmt.Println("Error unmarshaling Bitget response:", err)
+					return
+				}
+
+				// rawData를 KlineResp 구조체로 변환
+				var respData []bsml.KlineResp
+				for _, kline := range rawData.Data {
+					if len(kline) < 6 {
+						// 최소 6개의 필드가 있어야 함
+						continue
+					}
+
+					// 공통 필드들 (비트겟은 timestamp가 string으로 반환됨)
+					timestampStr := kline[0].(string)
+					timestamp, err := strconv.ParseInt(timestampStr, 10, 64) // string을 int64로 변환
+					if err != nil {
+						fmt.Println("Error parsing timestamp:", err)
+						continue
+					}
+					open := kline[1].(string)
+					high := kline[2].(string)
+					low := kline[3].(string)
+					close := kline[4].(string)
+					volume := kline[5].(string)
+
+					// KlineResp 구조체에 데이터를 할당
+					mu.Lock() // 응답을 안전하게 추가하기 위해 락을 사용
+					respData = append(respData, bsml.KlineResp{
+						Timestamp:           timestamp,
+						Open:                utility.ParseToFloat64(open),
+						High:                utility.ParseToFloat64(high),
+						Low:                 utility.ParseToFloat64(low),
+						Close:               utility.ParseToFloat64(close),
+						Volume:              utility.ParseToFloat64(volume),
+						CloseTime:           0,   // 비트겟에는 CloseTime이 없음
+						QuoteVolume:         0.0, // 비트겟에서는 quoteVolume을 사용할 수 없으므로 0으로 처리
+						NumberOfTrades:      0,   // 비트겟에는 NumberOfTrades가 없음
+						TakerBuyBaseVolume:  0.0, // 비트겟에는 TakerBuyBaseVolume이 없음
+						TakerBuyQuoteVolume: 0.0, // 비트겟에는 TakerBuyQuoteVolume이 없음
+					})
+				}
+				// 심볼별로 응답을 맵에 추가
+				mu.Lock()
+				respMap[symbolStr] = respData
+				mu.Unlock()
+			}
+		}(symbol.Symbol) // 고루틴에 symbol을 전달
 	}
 
-	return true, "", resp
+	// 모든 고루틴이 완료될 때까지 대기
+	wg.Wait()
+
+	// 결과 반환
+	return true, "", respMap
+}
+
+// KlineWebSocoketInq
+func KlineWebSocoketInqBinance(ReqInfo *bsml.WebSocketKlineRequest) (bool, string, bsml.WebSocketResponse) {
+
+	var resp bsml.WebSocketResponse
+
+	// 웹소켓 URL (BTCUSDT, 1분 간격)
+	url := "wss://stream.binance.com:9443/ws/btcusdt@kline_1m"
+
+	// 웹소켓 연결
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		return false, "websocket connect failed", resp
+	}
+	defer conn.Close()
+
+	fmt.Println("웹소켓 연결 성공")
+
+	// 웹소켓으로부터 메시지를 지속적으로 수신
+	for {
+		// 메시지 수신
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			return false, "", resp
+		}
+
+		// JSON 파싱
+		err = json.Unmarshal(message, &resp)
+		fmt.Println(err)
+		if err != nil {
+			return false, err.Error(), resp
+			continue
+		}
+
+		// Kline 데이터 출력
+		fmt.Printf("Timestamp: %d, Open: %s, Close: %s, High: %s, Low: %s, Volume: %s\n",
+			resp.Kline.Timestamp, resp.Kline.OpenPrice, resp.Kline.ClosePrice,
+			resp.Kline.HighPrice, resp.Kline.LowPrice, resp.Kline.Volume)
+	}
+}
+
+// 구독 메시지 구조체
+type SubscribeMessage struct {
+	Op   string `json:"op"`
+	Args []struct {
+		InstType string `json:"instType"`
+		Channel  string `json:"channel"`
+		InstId   string `json:"instId"`
+	} `json:"args"`
+}
+
+// WebSocket 연결 및 Kline 데이터 구독 함수
+func KlineWebSocoketInqBitget(ReqInfo *bsml.WebSocketKlineRequest) (bool, string, bsml.WebSocketResponse) {
+
+	var resp bsml.WebSocketResponse
+
+	const url = "wss://ws.bitget.com/v2/ws/public"
+	// WebSocket 연결
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		return false, "websocket connect failed", resp
+	}
+	defer conn.Close()
+
+	// Kline 데이터 및 Ticker 구독 메시지 생성
+	subscribeMessage := SubscribeMessage{
+		Op: "subscribe",
+		Args: []struct {
+			InstType string `json:"instType"`
+			Channel  string `json:"channel"`
+			InstId   string `json:"instId"`
+		}{
+			{
+				InstType: "SPOT",                      // 상품 종류
+				Channel:  "candle" + ReqInfo.Interval, // 예: "candle5m"으로 Kline 채널 설정
+				InstId:   ReqInfo.Symbol,              // 거래쌍, 예: "BTC-USDT"
+			},
+		},
+	}
+
+	// 구독 메시지 JSON으로 변환
+	subscribeMessageJSON, err := json.Marshal(subscribeMessage)
+	if err != nil {
+		return false, "websocket connect failed", resp
+	}
+
+	fmt.Println(subscribeMessageJSON)
+
+	// 구독 메시지 전송
+	err = conn.WriteMessage(websocket.TextMessage, subscribeMessageJSON)
+	if err != nil {
+		log.Fatal("구독 메시지 전송 실패:", err)
+	}
+	fmt.Println("BTCUSDT 5분 간격 Kline 데이터 구독 시작")
+
+	// 실시간 데이터 수신
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Fatal("메시지 수신 오류:", err)
+		}
+
+		// 수신된 메시지 출력 (그대로 출력하여 확인)
+		fmt.Printf("수신된 메시지: %s\n", message)
+	}
+}
+
+// 통합된 WebSocket 요청 처리 함수
+func KlineWebSocoketInq(ReqInfo *bsml.WebSocketKlineRequest) (bool, string, bsml.WebSocketResponse) {
+	if ReqInfo.ExchgNm == "binance" {
+
+		resultFlag, errMsg, resp := KlineWebSocoketInqBinance(ReqInfo)
+
+		return resultFlag, errMsg, resp
+	} else if ReqInfo.ExchgNm == "bitget" {
+
+		resultFlag, errMsg, resp := KlineWebSocoketInqBitget(ReqInfo)
+
+		return resultFlag, errMsg, resp
+	} else {
+		return false, "지원되지 않는 거래소 입니다.", bsml.WebSocketResponse{}
+	}
 }
 
 // 	// 2D 배열로 된 JSON 응답 파싱
--- busimodel/api_req_reply_info.go
+++ busimodel/api_req_reply_info.go
@@ -1,14 +1,20 @@
 package busimodel
 
+import "encoding/json"
+
 // KlineRequest : Kline API 요청의 파라미터들을 담는 구조체
 type KlineRequest struct {
-	ExchgNm   string `json:"exchange_name" binding:"required"` // 거래소 명
-	Symbol    string `json:"symbol" binding:"required"`        // 거래쌍 (ex: BTC-USDT)
-	Interval  string `json:"interval" binding:"required"`      // 간격 (ex: 1m, 5m, 1h...)
-	StartTime string `json:"start_time" binding:"omitempty"`   // 시작 시간
-	EndTime   string `json:"end_time" binding:"omitempty"`     // 종료 시간
-	TimeZone  string `json:"time_zone" binding:"omitempty"`    // Default: 0 (UTC)
-	Limit     int64  `json:"limit" binding:"omitempty"`        // Default 500; max 1000.
+	ExchgNm   string    `json:"exchange_name" binding:"required"` // 거래소 명
+	Symbols   []Symbols `json:"symbols" binding:"required"`       // 거래쌍 (ex: BTC-USDT)
+	Interval  string    `json:"interval" binding:"required"`      // 간격 (ex: 1m, 5m, 1h...)
+	StartTime string    `json:"start_time" binding:"omitempty"`   // 시작 시간
+	EndTime   string    `json:"end_time" binding:"omitempty"`     // 종료 시간
+	TimeZone  string    `json:"time_zone" binding:"omitempty"`    // Default: 0 (UTC)
+	Limit     int64     `json:"limit" binding:"omitempty"`        // Default 500; max 1000.
+}
+
+type Symbols struct {
+	Symbol string `json:"symbol" binding:"required"` // 거래쌍 (ex: BTC-USDT)
 }
 
 type KlineResp struct {
@@ -23,4 +29,36 @@ type KlineResp struct {
 	NumberOfTrades      int     `json:"number_of_trades,omitempty"`       // Number of trades (for Binance)
 	TakerBuyBaseVolume  float64 `json:"taker_buy_base_volume,omitempty"`  // Taker buy volume (for Binance)
 	TakerBuyQuoteVolume float64 `json:"taker_buy_quote_volume,omitempty"` // Taker buy volume in quote currency (for Binance)
+}
+
+// WebSocketKlineRequest : Kline API 요청의 파라미터들을 담는 구조체
+type WebSocketKlineRequest struct {
+	ExchgNm  string `json:"exchange_name" binding:"required"` // 거래소 명
+	Symbol   string `json:"symbol" binding:"required"`        // 거래쌍 (ex: BTC-USDT)
+	Interval string `json:"interval" binding:"required"`      // 간격 (ex: 1m, 5m, 1h...)
+}
+
+// Kline 구조체
+type KlineData struct {
+	Timestamp     int64       `json:"t"` // Kline start time
+	CloseTime     int64       `json:"T"` // Kline close time
+	Symbol        string      `json:"s"` // Symbol
+	Interval      string      `json:"i"` // Interval
+	OpenPrice     string      `json:"o"` // Open price
+	ClosePrice    string      `json:"c"` // Close price
+	HighPrice     string      `json:"h"` // High price
+	LowPrice      json.Number `json:"l"` // Low price
+	Volume        string      `json:"v"` // Base asset volume
+	TradesCount   int         `json:"n"` // Number of trades
+	IsClosed      bool        `json:"x"` // Is this kline closed?
+	QuoteVolume   string      `json:"q"` // Quote asset volume
+	TakerBuyBase  string      `json:"V"` // Taker buy base asset volume
+	TakerBuyQuote string      `json:"Q"` // Taker buy quote asset volume
+}
+
+type WebSocketResponse struct {
+	EventType string    `json:"e"` // Event type (always "kline")
+	EventTime int64     `json:"E"` // Event time
+	Symbol    string    `json:"s"` // Symbol (e.g., "BTCUSDT")
+	Kline     KlineData `json:"k"` // Kline data
 }
